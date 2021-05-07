510c28ebd13157336113107e0ac2f8ecff59350b
Luke074 <mendeslucas277@gmail.com>
2021-05-07 20:16:11 -0300

    projeto finalizado

--- tests/integration/manager.spec.js
+++ tests/integration/manager.spec.js
@@ -3,20 +3,55 @@ const request = require("supertest");
 //criar vairavel de ambient para rodar o teste no bd_teste
 
 const app = require("../../src/app");
+const connection = require("../../src/database/index");
 const {cpf} = require("cpf-cnpj-validator");
+const { response } = require("../../src/app");
+const truncate = require("./truncate");
 
 describe("MANAGER", () => {
-  it("é possivel criar um novo gerente", () => {
-    const response = request(app).post("/managers", {
-      name: "Rafael Leme",
-      cpf: cpf.generate(),
+  afterAll(() => {
+    connection.close();
+  });
+
+  beforeEach(async (done) => {
+    await truncate(connection.models)
+    done();
+  });
+
+  it("é possivel criar um novo gerente", async ()  => {
+    let cpfGenerate = cpf.generate();
+    let response = await request(app).post("/managers").send({
+      name: "Lucas",
+      cpf: cpfGenerate,
       email: "teste@gmail.com",
-      cellphone: "55119998208012",
+      cellphone: "5511998208012",
       password: "123456",
     });
 
     expect(response.ok).toBeTruthy();
     expect(response.body).toHaveProperty("id");
-    
   });
+
+  it("não é possivel cadastrar um gerente com cpf existente", async () => {
+    let cpfGenerate = cpf.generate();
+    let response = await request(app).post("/managers").send({
+      name: "Lucas",
+      cpf: cpfGenerate,
+      email: "teste@gmail.com",
+      cellphone: "5511998208012",
+      password: "123456",
+    });
+
+    response = await request(app).post("/managers").send({
+      name: "Pedro",
+      cpf: cpfGenerate,
+      email: "teste123@gmail.com",
+      cellphone: "5511998208012",
+      password: "123456",
+    });
+  });
+
+  expect(response.ok).toBeFalsy();
+  expect(response.body).toHaveProperty("error");
+  expect(response.body.error).toEqual("cpf already exists");
 });
